faf68612cdf2673b7069aba1a9871cf881d4f29e
ArttyLists <arttylists@gmail.com>
2024-09-15 15:10:08 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, send_file
+from gtts import gTTS
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/convert', methods=['POST'])
+def convert_text_to_speech():
+    text = request.form['text']
+    if text:
+        tts = gTTS(text=text, lang='en', slow=False)
+        audio_file = "static/speech.mp3"
+        tts.save(audio_file)
+        return send_file(audio_file, as_attachment=True)
+    return "Error: No text provided"
+
+if __name__ == '__main__':
+    app.run(debug=True)
